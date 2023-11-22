5db1c42f1242281bc88d3baedd809b72917f3104
ka92tevi <boserituparna2011@gmail.com>
2023-11-22 11:56:03 +0100

    Updated pipeline.sh

---
+++ project/pipeline.sh
@@ -0,0 +1,11 @@
+# Execute the data pipeline
+python automatedpipeline.py
+
+database_file="dbrituparna"
+
+# Check if the output file(s) exist
+if [ -f "$database_file" ]; then
+    echo "Yes! The database file exists."
+else
+    echo "No database file found!!"
+fi
