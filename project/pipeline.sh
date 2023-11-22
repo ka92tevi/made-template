2831d7ee83de7202d42bd7b725bc5bf4e5856471
ka92tevi <boserituparna2011@gmail.com>
2023-11-22 14:34:43 +0100

    Updated pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,11 +1,4 @@
+#!/bin/bash
+
 # Execute the data pipeline
-python automatedpipeline.py
-
-database_file="dbrituparna"
-
-# Check if the output file(s) exist
-if [ -f "$database_file" ]; then
-    echo "Yes! The database file exists."
-else
-    echo "No database file found!!"
-fi
+python D:\FAU\MADE\made-template\data\automatedpipeline.py
