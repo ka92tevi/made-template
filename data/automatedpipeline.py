1211f2edb8b080fab086141e8007942a9af566b2
ka92tevi <boserituparna2011@gmail.com>
2024-01-22 22:01:21 +0100

    Updated automatedpipeline

--- data/automatedpipeline.py
+++ data/automatedpipeline.py
@@ -2,15 +2,15 @@ import sqlite3
 import pandas as pd
 
 
-# Data_1 is Datasource 3 from project-plan.md
+# Data_1 is Datasource 1 from project-plan.md
 
-data_1 = pd.read_csv("D:/FAU/MADE/Bicycle_traffic_data_Cologne.csv") #datasource collect from https://offenedaten-koeln.de/sites/default/files/Radverkehr%20f%C3%BCr%20Offene%20Daten%20K%C3%B6ln%202013.csv
+data_1 = pd.read_csv("D:/FAU/MADE/GDP_per_capita_table.csv") #datasource collect from https://www.kaggle.com/datasets/samuelcortinhas/gdp-of-european-countries?select=GDP_per_capita_table.csv
 
 # Data_2 is Datasource 2 from project-plan.md
 
-data_2 = pd.read_csv("D:/FAU/MADE/Accident_statistics_Cologne.csv") #datasource collect from https://offenedaten-koeln.de/dataset/unfallstatistik-k%C3%B6ln/resource/a32e0d4a-8a29-4b18-a49d-a9e1966c8c6f#{}
+data_2 = pd.read_csv("D:/FAU/MADE/unemployment_rate.xlsx") #datasource collect from https://ec.europa.eu/eurostat/databrowser/view/une_rt_a__custom_9447392/default/table?lang=en
 
 conn = sqlite3.connect("data/dbrituparna.sqlite")
 
-data_1.to_sql("Bicycle", conn, if_exists="replace") 
-data_2.to_sql("Accident", conn, if_exists="replace") 
+data_1.to_sql("GDP Count", conn, if_exists="replace") 
+data_2.to_sql("Unemployment Rate", conn, if_exists="replace") 
