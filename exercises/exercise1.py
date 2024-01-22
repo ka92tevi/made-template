b3b71ea6b557b6fd17abd2ceeaf241aa42d3422f
ka92tevi <boserituparna2011@gmail.com>
2024-01-22 21:44:10 +0100

    update

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import String, Text, Integer, Float , DECIMAL
 
 #loading from data souce
-datas= pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',sep=";", on_bad_lines='skip')
+data= pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',sep=";", on_bad_lines='skip')
 
 #changing column types
 columnTypes = {'column_1': Integer, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
@@ -10,4 +10,4 @@ columnTypes = {'column_1': Integer, 'column_2': String, 'column_3': String, 'col
                'column_11': Text,'column_12': String, 'geo_punkt': DECIMAL}
 
 #saving table to sqlite_file
-datas.to_sql('airports','sqlite:///airports.sqlite', if_exists='replace', index=False)
+data.to_sql('airports','sqlite:///airports.sqlite', if_exists='replace', index=False)
